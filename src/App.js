c8b78fac911a730d3c3381d2ad6e9a4c58834064
caleb <cmcmillan050@gmail.com>
2024-09-01 11:33:38 -0400

    5ed commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 // react router dom
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 // pages
 import { Home, MealDetails, Error, Category } from "./pages/index";
 // components
@@ -9,30 +9,147 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
 import RandomMeals from './components/RandomMeals/RandomMeals';
 import { useAuth, AuthProvider } from './context/AuthContext';
+import Navbar from './components/Header/Navbar';
+import CreateMyPlate from './pages/CreateMyPlates/CreateMyPlates';
 
 function App() {
-  const { isAuthenticated } = useAuth();  // <-- This line should be used after the AuthProvider
+  const location = useLocation();
+  const isCreateMyPlatePage = location.pathname === '/create-my-plate';
 
   return (
-    <BrowserRouter>
-      <Header />
-      {isAuthenticated && <Sidebar />} {/* Show Sidebar only for logged-in users */}
+    <>
+      {!isCreateMyPlatePage && <Navbar />}
+      {!isCreateMyPlatePage && <Header />}
+      {!isCreateMyPlatePage && <Sidebar />}
+
       <Routes>
+        <Route path="/create-my-plate" element={<CreateMyPlate />} />
         <Route path="/" element={<Home />} />
         <Route path="/meal/:id" element={<MealDetails />} />
         <Route path="/meal/category/:name" element={<Category />} />
         <Route path="*" element={<Error />} />
       </Routes>
-      {isAuthenticated && <RandomMeals />} {/* Show RandomMeals only for logged-in users */}
-      <Footer />
-    </BrowserRouter>
+
+
+      {!isCreateMyPlatePage && <Footer />}
+    </>
   );
 }
 
 export default function RootApp() {
   return (
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.scss';
+// // react router dom
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // pages
+// import { Home, MealDetails, Error, Category } from "./pages/index";
+// // components
+// import Header from "./components/Header/Header";
+// import Sidebar from "./components/Sidebar/Sidebar";
+// import Footer from "./components/Footer/Footer";
+// import RandomMeals from './components/RandomMeals/RandomMeals';
+// import { useAuth, AuthProvider } from './context/AuthContext';
+// import Navbar from './components/Header/Navbar';
+// import CreateMyPlate from './pages/CreateMyPlates/CreateMyPlates';
+
+// function App() {
+//   const { isAuthenticated } = useAuth();  // <-- This line should be used after the AuthProvider
+
+//   return (
+//     <BrowserRouter>
+//     <Navbar/>
+//       <Header />
+//       {<Sidebar />} 
+//       <Routes>
+//       <Route path="/create-my-plate" element={<CreateMyPlate />} />
+//         <Route path="/" element={<Home />} />
+//         <Route path="/meal/:id" element={<MealDetails />} />
+//         <Route path="/meal/category/:name" element={<Category />} />
+//         <Route path="*" element={<Error />} />
+//       </Routes>
+//       {<RandomMeals />} 
+//       <Footer />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default function RootApp() {
+//   return (
+//     <AuthProvider>
+//       <App />
+//     </AuthProvider>
+//   );
+// }
